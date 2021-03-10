e35000dca8b3fa723ee9bc6037d7c7b1a3a90257
PRIYAM <priyam266german@gmail.com>
2021-03-10 19:41:31 +0530

    some changes in calendar page

--- lib/screens/calendar/calendar_screen.dart
+++ lib/screens/calendar/calendar_screen.dart
@@ -1,10 +1,37 @@
-import 'package:checkbox/screens/calendar/components/body.dart';
+// import 'package:checkbox/screens/calendar/components/body.dart';
 import 'package:flutter/material.dart';
-
 import '../../size_config.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   static String routeName = "/calendar";
+
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarController _controller;
+
+  Map<DateTime, List<dynamic>> _events;
+
+  List<dynamic> _selectedEvents;
+
+  TextEditingController _eventController;
+
+  SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+    _events = {};
+    _selectedEvents = [];
+    prefsData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +45,148 @@ class Calendar extends StatelessWidget {
         ),
         toolbarHeight: SizeConfig.screenHeight * 0.1,
       ),
-      body: Body(),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TableCalendar(
+              events: _events,
+              calendarController: _controller,
+              calendarStyle: CalendarStyle(
+                canEventMarkersOverflow: true,
+                todayColor: Colors.orange,
+                selectedColor: Theme.of(context).primaryColor,
+                todayStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0,
+                    color: Colors.white),
+              ),
+              headerStyle: HeaderStyle(
+                centerHeaderTitle: true,
+                formatButtonVisible: false,
+              ),
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events) => Container(
+                  margin: const EdgeInsets.all(4.0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Text(
+                    date.day.toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                todayDayBuilder: (context, date, events) => Container(
+                  margin: const EdgeInsets.all(4.0),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10.0)),
+                  child: Text(
+                    date.day.toString(),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              onDaySelected: (date, events, holidays) {
+                setState(() {
+                  _selectedEvents = events;
+                });
+              },
+            ),
+            ..._selectedEvents.map(
+              (event) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 20,
+                  width: MediaQuery.of(context).size.width / 2,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey)),
+                  child: Center(
+                    child: Text(
+                      event,
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        child: Icon(Icons.add),
+        onPressed: _showAddDialog,
+      ),
     );
+  }
+
+  _showAddDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              backgroundColor: Colors.white70,
+              title: Text("Add Events"),
+              content: TextField(
+                controller: _eventController,
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text(
+                    "Save",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    if (_eventController.text.isEmpty) return;
+                    setState(() {
+                      if (_events[_controller.selectedDay] != null) {
+                        _events[_controller.selectedDay]
+                            .add(_eventController.text);
+                      } else {
+                        _events[_controller.selectedDay] = [
+                          _eventController.text
+                        ];
+                      }
+                      prefs.setString(
+                          "events", json.encode(encodeMap(_events)));
+                      _eventController.clear();
+                      Navigator.pop(context);
+                    });
+                  },
+                )
+              ],
+            ));
+  }
+
+  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+    Map<String, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+    Map<DateTime, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
+  }
+
+  prefsData() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _events = Map<DateTime, List<dynamic>>.from(
+          decodeMap(json.decode(prefs.getString("events") ?? "{}")));
+    });
   }
 }
--- lib/screens/calendar/components/body.dart
+++ lib/screens/calendar/components/body.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -6,12 +10,159 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  CalendarController _controller;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+  TextEditingController _eventController;
+  SharedPreferences prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+    _events = {};
+    _selectedEvents = [];
+    prefsData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Calendar"),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TableCalendar(
+            events: _events,
+            calendarController: _controller,
+            calendarStyle: CalendarStyle(
+              canEventMarkersOverflow: true,
+              todayColor: Colors.orange,
+              selectedColor: Theme.of(context).primaryColor,
+              todayStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18.0,
+                  color: Colors.white),
+            ),
+            headerStyle: HeaderStyle(
+              centerHeaderTitle: true,
+              formatButtonVisible: false,
+            ),
+            builders: CalendarBuilders(
+              selectedDayBuilder: (context, date, events) => Container(
+                margin: const EdgeInsets.all(4.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Text(
+                  date.day.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              todayDayBuilder: (context, date, events) => Container(
+                margin: const EdgeInsets.all(4.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Text(
+                  date.day.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            onDaySelected: (date, events, holidays) {
+              setState(() {
+                _selectedEvents = events;
+              });
+            },
+          ),
+          ..._selectedEvents.map(
+            (event) => Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 20,
+                width: MediaQuery.of(context).size.width / 2,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey)),
+                child: Center(
+                  child: Text(
+                    event,
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  _showAddDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              backgroundColor: Colors.white70,
+              title: Text("Add Events"),
+              content: TextField(
+                controller: _eventController,
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text(
+                    "Save",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    if (_eventController.text.isEmpty) return;
+                    setState(() {
+                      if (_events[_controller.selectedDay] != null) {
+                        _events[_controller.selectedDay]
+                            .add(_eventController.text);
+                      } else {
+                        _events[_controller.selectedDay] = [
+                          _eventController.text
+                        ];
+                      }
+                      prefs.setString(
+                          "events", json.encode(encodeMap(_events)));
+                      _eventController.clear();
+                      Navigator.pop(context);
+                    });
+                  },
+                )
+              ],
+            ));
+  }
+
+  Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+    Map<String, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[key.toString()] = map[key];
+    });
+    return newMap;
+  }
+
+  Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+    Map<DateTime, dynamic> newMap = {};
+    map.forEach((key, value) {
+      newMap[DateTime.parse(key)] = map[key];
+    });
+    return newMap;
+  }
+
+  prefsData() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _events = Map<DateTime, List<dynamic>>.from(
+          decodeMap(json.decode(prefs.getString("events") ?? "{}")));
+    });
   }
 }
